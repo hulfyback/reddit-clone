22477cbe0705c808314380cb123a4c4bef590052
Zsolt Molnár <hulfyback@gmail.com>
2019-06-23 13:36:52 +0200

    feat(reddig-clone): authenticate by Google Account
    create WebServiceConfiguration class
    add n ew RestController returning by Principal object

---
+++ src/main/java/com/greenfoxacademy/redditclone/configuration/RedditCloneWebSecurityConfiguration.java
@@ -0,0 +1,11 @@
+package com.greenfoxacademy.redditclone.configuration;
+
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableOAuth2Sso
+public class RedditCloneWebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+}
--- src/main/java/com/greenfoxacademy/redditclone/controller/PostRestController.java
+++ src/main/java/com/greenfoxacademy/redditclone/controller/PostRestController.java
@@ -3,6 +3,7 @@ package com.greenfoxacademy.redditclone.controller;
 
 import com.greenfoxacademy.redditclone.model.Post;
 import com.greenfoxacademy.redditclone.service.IPostService;
+import java.security.Principal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +21,11 @@ public class PostRestController {
   @GetMapping("/api/posts")
   public List<Post> listAllPosts() {
     return postService.getAllPosts();
+  }
+
+  @GetMapping("/user")
+  public Principal user(Principal principal) {
+    return principal;
   }
 }
 
